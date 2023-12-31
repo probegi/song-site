d5fadeb41b1686baa73ee8597736b5ca1d2221bf
probegi <chhr22012@g.nihon-u.ac.jp>
2023-12-31 15:15:10 +0900

    css追加

--- src/App.jsx
+++ src/App.jsx
@@ -19,34 +19,64 @@ export default function App() {
   }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="customtitle">曲のタイトルを入力:</label>
-          <input
-            type="text"
-            id="customtitle"
-            className="input is-info is-small"
-            placeholder=""
-          />
+    <>
+      <header className="hero is-primary is-bold">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title">動画・歌詞検索サイト</h1>
+          </div>
         </div>
-        <div>
-          <label htmlFor="customname">歌手名を入力:</label>
-          <input
-            type="text"
-            id="customname"
-            className="input is-info is-small"
-            placeholder=""
-          />
+      </header>
+      <main>
+        <section className="section">
+          <div className="container">
+            <form className="enter-block" onSubmit={handleSubmit}>
+              <div>
+                <label htmlFor="customtitle">曲のタイトルを入力:</label>
+                <input
+                  type="text"
+                  id="customtitle"
+                  className="input is-info is-small"
+                  placeholder=""
+                />
+              </div>
+              <div>
+                <label htmlFor="customname">歌手名を入力:</label>
+                <input
+                  type="text"
+                  id="customname"
+                  className="input is-info is-small"
+                  placeholder=""
+                />
+              </div>
+              <div>
+                <button
+                  className="button is-link is-light is-small"
+                  type="submit"
+                >
+                  検索
+                </button>
+              </div>
+            </form>
+            <div className="music-result">
+              <div>
+                <YoutubeItem id={id} />
+              </div>
+              <div>
+                <iframe src={url}></iframe>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+      <footer className="footer">
+        <div className="content has-text-centered">
+          <p>
+            from&ensp;
+            <a href="https://nationalize.io/">nationalize.io</a>
+          </p>
         </div>
-        <div>
-          <button>検索</button>
-        </div>
-      </form>
-      <div>
-        <YoutubeItem id={id} />
-      </div>
-      <iframe src={url}></iframe>
-    </div>
+      </footer>
+    </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import "bulma/css/bulma.css";
 
 createRoot(document.querySelector("#content")).render(<App />);
